54ea296b530a838a21750c04452f4afbe5efb5e5
zNotChill <znotchill@znci.dev>
2023-09-05 12:44:22 +0100

    the ENTIRE THING

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 import PlayerServers from "./playerservers/index";
+import { consola } from "consola";
 
 import dotenv from 'dotenv';
 
@@ -11,12 +12,48 @@ let { CUBED_USER, CUBED_PASS } = process.env;
 
 if(!CUBED_USER || !CUBED_PASS) throw new Error('Missing CUBED_USER or CUBED_PASS in .env');
 
-playerservers.login(CUBED_USER, CUBED_PASS).then(() => {
-  console.log('Logged in!');
+const motds = [
+  "&c&lTOP SECRET SERVER",
+  "&a&lJOIN FOR FREE OP!",
+  "&a&lLEAKED STAFF DOCS",
+  "&b&lNEW BEDBOX JOIN NOW!!",
+  "&a&lzNotChill was here!",
+  "&a&l_EliteFestell was here!",
+  "&a&lMyNameIs12345 was here!",
+  "&a&lgabitza1234 was here!",
+  "&a&lJeroeno_Boy was here!",
+  "&a&lIDuckz_ was here!",
+  "&a&lRIP SHITBOX :PENSIVE:",
+  "&a&lMANGO IS THE BEST SERVER!",
+  "&a&lBetter than Notchbox!",
+  "&b&lfree vbucks",
+  "&b&lfree robux",
+  "&b&lfree minecraft account",
+  "&b&lCUSTOM SKRIPTED FEATURES!",
+  "&b&lCUSTOM PLUGINS!",
+  "&a&lSERVER RELEASED!",
+  "&8[&c&lNEW&8] &c&lPlugin added",
+  "&8[&c&lNEW&8] &c&lPlugin remove",
+  "&8[&c&lNEW&8] &c&lPlugin update",
+  "&8[&c&lNEW&8] &c&lPlugin fixed",
+  "&6LEAKED STAFF IP",
+  "&aILLEGAL MODIFICATIONS ALLOWED",
+]
 
-  const servers = playerservers.getServers();
+playerservers.login({
+  username: CUBED_USER,
+  password: CUBED_PASS,
+}, {
+  verbose: true,
+}).then(async (res) => {
+  console.log('Logged in as ' + res.username);
+
+  const select = await playerservers.selectServer("Mango");
   
-
+  setInterval(async () => {
+    await playerservers.setMOTD(motds[Math.floor(Math.random() * motds.length)]);
+  }, 10000);
+  
 }).catch((err) => {
   console.error(err);
 });
